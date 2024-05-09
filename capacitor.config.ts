9530e90656e58054a083ff975991e4df467d04c8
Amy Mcevoy <G00413220@atu.ie>
2024-05-09 14:14:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'weatherA',
+  webDir: 'www'
+};
+
+export default config;
